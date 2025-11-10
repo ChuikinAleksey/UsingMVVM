6f4376a52ef2730583ec0a463740d8f3a3473246
AleksChuykin <chuikin.aleksey.7@gmail.com>
2025-11-10 16:53:02 +0300

    Добавление еще более четкого разделения на MVVM модель

--- app/src/main/java/com/example/usingmvvm/CounterViewModel.kt
+++ app/src/main/java/com/example/usingmvvm/CounterViewModel.kt
@@ -4,17 +4,19 @@ import androidx.lifecycle.ViewModel
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 
-class CounterViewModel : ViewModel() {
-
-    private val _count = mutableStateOf(0)
+class CounterViewModel() : ViewModel() {
+    private val _repository: CounterRepository = CounterRepository()
+    private val _count = mutableStateOf(_repository.getCounter().count)
 
     val count: MutableState<Int> = _count
 
     fun increment() {
-        _count.value++
+        _repository.incrementCounter()
+        _count.value = _repository.getCounter().count
     }
 
     fun decrement() {
-        _count.value--
+        _repository.decrementCounter()
+        _count.value = _repository.getCounter().count
     }
 }
